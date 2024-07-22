e6e97f292ad14a2059beeca8a8a5f6d4e11d4699
raoarhamfaisal <arhamfaisal780@gmail.com>
2024-07-22 22:17:45 +0500

    user model added and assign user to req object

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
 const mongoose = require("mongoose");
-// const User = require("./models/user");`
+const User = require("./models/user");
 
 const app = express();
 
@@ -18,14 +18,14 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use((req, res, next) => {
-// User.findById("669a8a78ca9208f88d5b49f5")
-//   .then((user) => {
-//     req.user = new User(user.name, user.email, user.cart, user._id);
-//     next();
-//   })
-//   .catch((err) => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById("669e93d5194ad4ce3040919e")
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -37,7 +37,23 @@ mongoose
     "mongodb+srv://arhamfaisal780:Arham123.@cluster0.rzdbkky.mongodb.net/shop?retryWrites=true&w=majority&appName=Cluster0"
   )
   .then((res) => {
+    User.findOne()
+      .then((user) => {
+        if (!user) {
+          const newUser = new User({
+            name: "Arham",
+            email: "arhamfaisal780@gmail.com",
+            cart: {
+              items: [],
+            },
+          });
+          newUser.save();
+        }
+        app.listen(3000);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
     console.log(res, "connected");
-    app.listen(3000);
   })
   .catch((err) => console.log(err));
