676da21d0ea913ee75214c447051fd6109d336fe
raoarhamfaisal <arhamfaisal780@gmail.com>
2024-07-26 02:23:51 +0500

    setup with mailtrap(for only testing purposes)

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,25 +1,41 @@
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
-const User = require('../models/user');
+const User = require("../models/user");
+
+// Import the Nodemailer library
+const nodemailer = require("nodemailer");
+
+// Create a transporter object
+const transporter = nodemailer.createTransport({
+  host: "sandbox.smtp.mailtrap.io",
+  port: 587,
+  secure: false, // use SSL
+  auth: {
+    user: "4058f82ed54e54",
+    pass: "294688f0eba5c2",
+  },
+});
+
+// Send the email
 
 exports.getLogin = (req, res, next) => {
-  let message = req.flash('error');
+  let message = req.flash("error");
   if (message.length > 0) {
     message = message[0];
   } else {
     message = null;
   }
-  res.render('auth/login', {
-    path: '/login',
-    pageTitle: 'Login',
-    errorMessage: message
+  res.render("auth/login", {
+    path: "/login",
+    pageTitle: "Login",
+    errorMessage: message,
   });
 };
 
 exports.getSignup = (req, res, next) => {
-  res.render('auth/signup', {
-    path: '/signup',
-    pageTitle: 'Signup'
+  res.render("auth/signup", {
+    path: "/signup",
+    pageTitle: "Signup",
   });
 };
 
@@ -27,30 +43,30 @@ exports.postLogin = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   User.findOne({ email: email })
-    .then(user => {
+    .then((user) => {
       if (!user) {
-        req.flash('error', 'Invalid email or password.');
-        return res.redirect('/login');
+        req.flash("error", "Invalid email or password.");
+        return res.redirect("/login");
       }
       bcrypt
         .compare(password, user.password)
-        .then(doMatch => {
+        .then((doMatch) => {
           if (doMatch) {
             req.session.isLoggedIn = true;
             req.session.user = user;
-            return req.session.save(err => {
+            return req.session.save((err) => {
               console.log(err);
-              res.redirect('/');
+              res.redirect("/");
             });
           }
-          res.redirect('/login');
+          res.redirect("/login");
         })
-        .catch(err => {
+        .catch((err) => {
           console.log(err);
-          res.redirect('/login');
+          res.redirect("/login");
         });
     })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 };
 
 exports.postSignup = (req, res, next) => {
@@ -58,32 +74,48 @@ exports.postSignup = (req, res, next) => {
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
   User.findOne({ email: email })
-    .then(userDoc => {
+    .then((userDoc) => {
       if (userDoc) {
-        return res.redirect('/signup');
+        return res.redirect("/signup");
       }
       return bcrypt
         .hash(password, 12)
-        .then(hashedPassword => {
+        .then((hashedPassword) => {
           const user = new User({
             email: email,
             password: hashedPassword,
-            cart: { items: [] }
+            cart: { items: [] },
           });
           return user.save();
         })
-        .then(result => {
-          res.redirect('/login');
+        .then((result) => {
+          // Configure the mailoptions object
+          const mailOptions = {
+            from: "arhamfaisal780@gmail.com",
+            to: email,
+            subject: "Successfully Signed Up",
+            text: "You are signed up to arhamfaisal",
+            // html: "<h1>Successfully Signed Up</h1>",
+          };
+
+          transporter.sendMail(mailOptions, function (error, info) {
+            if (error) {
+              console.log("Error:", error);
+            } else {
+              console.log("Email sent:", info.response);
+            }
+          });
+          res.redirect("/login");
         });
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
     });
 };
 
 exports.postLogout = (req, res, next) => {
-  req.session.destroy(err => {
+  req.session.destroy((err) => {
     console.log(err);
-    res.redirect('/');
+    res.redirect("/");
   });
 };
